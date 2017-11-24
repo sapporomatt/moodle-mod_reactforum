3beabf4933e108b12035fb467dea2c0a9f06f7dd
Ponlawat <ponlawat_w@cmu.ac.th>
2017-11-24 16:15:37 +0700

    Remove course_created event observer
    - Disable event observer which creates reactforum instance automatically when new course is created.

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017110100;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2017112400;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2017050500;       // Requires this Moodle version
 $plugin->component = 'mod_reactforum';      // Full name of the plugin (used for diagnostics)
